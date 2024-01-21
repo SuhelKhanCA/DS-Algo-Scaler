709043a884f4a3445aa60111f64cf0b3163998e2
suhelkhanca <suhel.hata@gmail.com>
2024-01-21 20:05:18 +0530

    corrected code of array insertion

--- NPTEL/Lec12/Lec12/Insertion1DArray.java
+++ NPTEL/Lec12/Lec12/Insertion1DArray.java
@@ -10,6 +10,7 @@ public class Insertion1DArray {
         if(size > 100){
             System.out.println("Could not initiallize..");
             // return 0;
+            // break;
         }
         this.size = size;
         Scanner in = new Scanner(System.in); // to read from keyboard
@@ -19,7 +20,13 @@ public class Insertion1DArray {
             a[i] = in.nextInt(); //load the ith entry
         }
         System.out.println("Capacity = " + a.length + " -- Size = "+ size);
-        System.out.println(Arrays.toString(a)); // display the array on the screen
+        // System.out.println(Arrays.toString(a)); // display the array on the screen
+        System.out.println("Array before insertion is : ");
+        for(int i = 0; i <size; i++){
+            System.out.print(a[i]+ " ");
+        }
+        System.out.println();
+
     }
 
     public  void insert(int loc){ // insertion at the location of the array === static
@@ -34,25 +41,32 @@ public class Insertion1DArray {
         Scanner in = new Scanner(System.in); // to read from the keyboard
         System.out.println("Enter the item to be inserted: ");
         int item = in.nextInt();
-        for(int i = loc; i < a.length-1; i++)
-            a[i+1] = a[i]; // shift right
+        for(int i = size; i >= loc; i--){
+            // int tmp = a[i];
+            // a[i+1] = tmp; // shift right
+            a[i+1] = a[i];
+        }
         a[loc] = item;
 
         System.out.println("Capacity = "+ a.length + " -- Size = "+size);
-        System.out.println(Arrays.toString(a)); // Display the elements after insertion
+        // System.out.println(Arrays.toString(a)); // Display the elements after insertion
+        for(int i = 0; i <= size; i++){
+            System.out.print(a[i] + " ");
+        }
         in.close();
     }
 // main method
     public static void main(String[] args) {
         Insertion1DArray obj1 = new Insertion1DArray();
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the size of the array..");
+        System.out.print("Enter the size of the array : ");
         int size = in.nextInt();
         obj1.create(size);
 
         // call for insertion
-        System.out.println("Enter the location at which elements to be inserted: ");
+        System.out.print("Enter the location at which elements to be inserted: ");
         int loc = in.nextInt();
         obj1.insert(loc);
+        in.close();
     }
 }
