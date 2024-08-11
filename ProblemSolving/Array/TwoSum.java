cfbe6780cabb1a8d1ea5242d96a0b49d6facbeba
suhelkhanca <suhel.hata@gmail.com>
2024-08-11 22:19:48 +0530

    Indexes of two elements having sum = target given

---
+++ ProblemSolving/Array/TwoSum.java
@@ -0,0 +1,27 @@
+package ProblemSolving.Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+class TwoSum { // Indexes of two elements having sum = target given
+
+    public static void main(String[] args) {
+        int arr[] = {11, 3, 7,9, 14, 2};
+        int target = 17;
+        int[] ans = new int[2];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int sno = target - arr[i];
+            if (map.containsKey(sno)) {
+                ans[0] = map.get(sno);
+                ans[1] = i;
+                break;
+            }
+            map.put(arr[i], i);
+        }
+
+        System.out.println("Indexes : " + Arrays.toString(ans));
+    }
+}
