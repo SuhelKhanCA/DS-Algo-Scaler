a124192f1ff4975391dcab788bcaa79ca88dc0b9
suhelkhanca <suhel.hata@gmail.com>
2025-02-12 17:58:24 +0530

    smallest number example

--- ProblemSolving/Array/SmallestNumber1.java
+++ ProblemSolving/Array/SmallestNumber1.java
@@ -1,34 +1,47 @@
 package ProblemSolving.Array;
 // https://www.geeksforgeeks.org/problems/digit-multiplier3000/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
 public class SmallestNumber1 {
 
-    static void findSmallest(int n) {
-        int hash[] = new int[10];
+    static int findSmallest(int n) {
+        // int hash[] = new int[10];
 
+        // for (int i = 9; i > 1; i--) {
+        //     while (n % i == 0 && n > 0) {
+        //         hash[i]++;
+        //         n = n / i;
+        //     }
+        // }
+        // System.out.println(Arrays.toString(hash));
+
+        // int num = 0;
+        // for (int i = 0; i < hash.length; i++) {
+        //     if (hash[i] != 0) {
+        //         while (hash[i] > 0) {
+        //             num = num * 10 + i;
+        //             hash[i]--;
+        //         }
+        //     }
+        // }
+        // System.out.println(num);
+
+        String ans = "";
         for (int i = 9; i > 1; i--) {
-            while (n % i == 0 && n > 0) {
-                hash[i]++;
+            while (n % i == 0) {
                 n = n / i;
+                ans = i + ans;
             }
         }
-        System.out.println(Arrays.toString(hash));
+        if (n != 1) {
+            return -1;
+        }
 
-        int num = 0;
-        for (int i = 0; i < hash.length; i++) {
-            if (hash[i] != 0) {
-                while (hash[i] > 0) {
-                    num = num * 10 + i;
-                    hash[i]--;
-                }
-            }
-        }
-        System.out.println(num);
+        return Integer.parseInt(ans);
     }
     public static void main(String[] args) {
-        int N = 81000;
-        findSmallest(N);
+        int n = 100;
+       System.out.println(findSmallest(n));
     }
 }
