40e250201dd361b907efdb3ef352a2e7dbc80e1f
suhelkhanca <suhel.hata@gmail.com>
2025-02-12 16:17:15 +0530

    2 sum problem

--- ProblemSolving/Array/TwoSum.java
+++ ProblemSolving/Array/TwoSum.java
@@ -1,15 +1,36 @@
 package ProblemSolving.Array;
-
+// https://leetcode.com/problems/two-sum/description/
 import java.util.Arrays;
 import java.util.HashMap;
 
-class TwoSum { // Indexes of two elements having sum = target given
+class Pair {
+    int i;
+    int j;
 
-    public static void main(String[] args) {
-        int arr[] = {11, 3, 7,9, 14, 2};
-        int target = 17;
+    Pair(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
+
+class TwoSum { // Indexes, of two elements having sum = target given
+
+    // Brute-force  O(n^2)
+    public static Pair findIndx(int[] a, int tar) {
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] + a[j] == tar) {
+                    return new Pair(i, j);
+                }
+            }
+        }
+        return new Pair(-1, -1);
+    }
+
+    // Using HashMap : O(n)
+    public static void findIndx2(int[] arr, int target) {
         int[] ans = new int[2];
-
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < arr.length; i++) {
@@ -23,5 +44,14 @@ class TwoSum { // Indexes of two elements having sum = target given
         }
 
         System.out.println("Indexes : " + Arrays.toString(ans));
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {11, 3, 7,9, 14, 2};
+        int target = 17;
+        Pair ans2 = findIndx(arr, target);
+        System.out.println("Indices are : "+ ans2.i + ", " + ans2.j);
+        
+        findIndx2(arr, target);
     }
 }
